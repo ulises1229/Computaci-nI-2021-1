6ea69787dddc4f2ced24f4d69456089f60e5128b
Ulises Olivares <uolivares@gdl.cinvestav.mx>
2020-11-03 18:24:37 -0600

    arreglos

--- code/arreglos/main.cpp
+++ code/arreglos/main.cpp
@@ -1,22 +1,45 @@
+// Author: Ulises Olivares
+// uolivaes@unam.mx
 
 #include <iostream>
 #include <array>
-#include <ctime>
-#include <cstdlib>
+#include <time.h>
+#include <stdlib.h>
+
+#define N 10
 
 using namespace std;
 
-float sumarNumeros(float a, float b);
+// Declaración de métodos
+void inicializarArreglo(array<int, N> &A);
+void imprimirArreglo(array<int, N> A);
 
 
 int main() {
+    //definición de un arreglo 1D usamos la variable N predefinida la inicio del programa
+    array<int, N> A;
 
-    int a {0},;
+    // Inicializar arreglo
+    inicializarArreglo(A);
 
+    // Imprimir arreglo
+    imprimirArreglo(A);
 
     return 0;
 }
 
-float sumarNumeros(float a, float b){
-    return (a + b);
+void inicializarArreglo(array<int, N> &A){
+    for(int i = 0; i<A.size(); i++){
+        // Inicializar arreglo con números del 1 al 100
+        A[i] = (rand() % 100) + 1;
+    }
+}
+
+// Paso de parametros por referencia
+void imprimirArreglo(array<int, N> A){
+    cout<< "Los elementos del arreglo son: "<< endl;
+    for(int i = 0; i<A.size(); i++){
+        cout<< A[i] << " ";
+    }
+    cout<< endl;
 }
